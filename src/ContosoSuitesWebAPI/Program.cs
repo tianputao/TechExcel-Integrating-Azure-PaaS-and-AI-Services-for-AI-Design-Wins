eefa48c453395c752a2f2225e80fc81d360990e2
Tarhone <fantian@microsoft.com>
2024-11-07 14:36:52 +0000

    samentic kernel function call

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -8,8 +8,18 @@ using Microsoft.Data.SqlClient;
 using Azure.AI.OpenAI;
 using Azure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+     .AddUserSecrets<Program>()
+     .AddEnvironmentVariables()
+     .Build();
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -39,6 +49,18 @@ builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
     var client = new AzureOpenAIClient(endpoint, credentials);
     return client;
 });
+
+builder.Services.AddSingleton<Kernel>((_) =>
+ {
+     IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+     kernelBuilder.AddAzureOpenAIChatCompletion(
+         deploymentName: builder.Configuration["AzureOpenAI:DeploymentName"]!,
+         endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+         apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+     );
+     kernelBuilder.Plugins.AddFromType<DatabaseService>();
+     return kernelBuilder.Build();
+ });
 
 var app = builder.Build();
 
@@ -90,13 +112,19 @@ app.MapGet("/", async () =>
 
 // This endpoint is used to send a message to the Azure OpenAI endpoint.
 app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
-{
-    var message = await Task.FromResult(request.Form["message"]);
-    
-    return "This endpoint is not yet available.";
-})
-    .WithName("Chat")
-    .WithOpenApi();
+ {
+     var message = await Task.FromResult(request.Form["message"]);
+     var kernel = app.Services.GetRequiredService<Kernel>();
+     var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+     var executionSettings = new OpenAIPromptExecutionSettings
+     {
+         ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+     };
+     var response = await chatCompletionService.GetChatMessageContentAsync(message.ToString(), executionSettings, kernel);
+     return response?.Content!;
+ })
+     .WithName("Chat")
+     .WithOpenApi();
 
 // This endpoint is used to vectorize a text string.
 // We will use this to generate embeddings for the maintenance request text.
